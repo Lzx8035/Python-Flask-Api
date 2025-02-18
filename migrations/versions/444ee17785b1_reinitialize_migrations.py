39e4e1214131a77d340c415fc7aa3fd2dc6857da
lzx8035 <lzx8035@gmail.com>
2025-02-18 02:05:15 -0800

    Fix migrations after deleting alembic_version

--- migrations/versions/444ee17785b1_reinitialize_migrations.py
+++ migrations/versions/444ee17785b1_reinitialize_migrations.py
@@ -1,8 +1,8 @@
-"""Initial migration
+"""Reinitialize migrations
 
-Revision ID: 520fa39685dc
+Revision ID: 444ee17785b1
 Revises: 
-Create Date: 2025-02-16 08:06:11.987932
+Create Date: 2025-02-18 10:03:45.006328
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '520fa39685dc'
+revision = '444ee17785b1'
 down_revision = None
 branch_labels = None
 depends_on = None
